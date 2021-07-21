e8eb4a006cc59d894bf4bf47d147f4661e98b6f8
Lucy <lucyperopero@gmail.com>
2021-07-21 14:41:02 -0300

    github actions

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,335 @@
-use std::env;
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    fmt::Write,
+    sync::Arc,
+};
 
-use std::path::Path;
+use serenity::prelude::*;
+use serenity::{
+    async_trait,
+    client::bridge::gateway::ShardManager,
+    framework::standard::{
+        buckets::{LimitedFor, RevertBucket},
+        help_commands,
+        macros::{check, command, group, help, hook},
+        Args, CommandGroup, CommandOptions, CommandResult, DispatchError, HelpOptions, Reason,
+        StandardFramework,
+    },
+    http::Http,
+    model::{
+        channel::{Channel, Message},
+        gateway::Ready,
+        id::UserId,
+        permissions::Permissions,
+    },
+    utils::Color,
+};
 
-use serenity::{async_trait, http::AttachmentType, model::{channel::Message, gateway::Ready}, prelude::*, utils::MessageBuilder};
+use rand::prelude::*;
+use tokio::sync::Mutex;
+
+// A container type is created for inserting into the Client's `data`, which
+// allows for data to be accessible across all events and framework commands, or
+// anywhere else that has a copy of the `data` Arc.
+struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
+
+struct CommandCounter;
+
+impl TypeMapKey for CommandCounter {
+    type Value = HashMap<String, u64>;
+}
+
+#[derive(Debug)]
+struct BlitzQuote {
+    quote: String,
+    author: String,
+}
+
+struct BlitzQuoteContainer;
+
+impl TypeMapKey for BlitzQuoteContainer {
+    type Value = Vec<BlitzQuote>;
+}
+
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-// The create message builder allows you to easily create embeds and messages
-            // using a builder syntax.
-            // This example will create a message that says "Hello, World!", with an embed that has
-            // a title, description, three fields, and a footer.
-            let msg = msg
-                .channel_id
-                .send_message(&ctx.http, |m| {
-                    m.embed(|e| {
-                        e.title("This is a title");
-                        e.description("This is a description");
-                        e.fields(vec![
-                            ("This is the first field", "This is a field body", true),
-                            ("This is the second field", "Both of these fields are inline", true),
-                        ]);
-                        e.field("This is the third field", "This is not an inline field", false);
-                        e.footer(|f| {
-                            f.text("This is a footer");
-
-                            f
-                        });
-
-                        e
-                    });
-                    m
-                })
-                .await;
-
-            if let Err(why) = msg {
-                println!("Error sending message: {:?}", why);
-            }        } else if msg.content == ".pics" {
-            
-
-            if let Err(why) = msg.channel_id.say(&ctx.http, "https://cdn.discordapp.com/attachments/846791487101730919/864959475709378560/unknown.png").await {
-                println!("Error sending message: {:?}", why);
-            }
-            
-            
-        }
-    }
-
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
 }
 
+#[group]
+#[commands(commands, blitz)]
+struct General;
+
+#[group]
+// Sets multiple prefixes for a group.
+// This requires us to call commands in this group
+// via `~emoji` (or `~em`) instead of just `~`.
+#[prefixes("emoji", "em")]
+// Set a description to appear if a user wants to display a single group
+// e.g. via help using the group-name or one of its prefixes.
+#[description = "A group with commands providing an emoji as response."]
+// Summary only appears when listing multiple groups.
+#[summary = "Do emoji fun!"]
+// Sets a command that will be executed if only a group-prefix was passed.
+#[default_command(bird)]
+#[commands(cat, dog)]
+struct Emoji;
+
+#[group]
+// Sets a single prefix for this group.
+// So one has to call commands in this group
+// via `~math` instead of just `~`.
+#[prefix = "math"]
+#[commands(multiply)]
+struct Math;
+
+#[hook]
+async fn before(ctx: &Context, msg: &Message, command_name: &str) -> bool {
+    println!(
+        "Got command '{}' by user '{}'",
+        command_name, msg.author.name
+    );
+
+    // Increment the number of times this command has been run once. If
+    // the command's name does not exist in the counter, add a default
+    // value of 0.
+    let mut data = ctx.data.write().await;
+    let counter = data
+        .get_mut::<CommandCounter>()
+        .expect("Expected CommandCounter in TypeMap.");
+    let entry = counter.entry(command_name.to_string()).or_insert(0);
+    *entry += 1;
+
+    true // if `before` returns false, command processing doesn't happen.
+}
+
+#[hook]
+async fn after(_ctx: &Context, _msg: &Message, command_name: &str, command_result: CommandResult) {
+    match command_result {
+        Ok(()) => println!("Processed command '{}'", command_name),
+        Err(why) => println!("Command '{}' returned error {:?}", command_name, why),
+    }
+}
+
+#[hook]
+async fn unknown_command(_ctx: &Context, _msg: &Message, unknown_command_name: &str) {
+    println!("Could not find command named '{}'", unknown_command_name);
+}
+
 #[tokio::main]
 async fn main() {
-
     // Configure the client with your Discord bot token in the environment.
     let token = env::var("CUTE_BOT_TOKEN").expect("Expected a token in the environment");
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client =
-        Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
+    let http = Http::new_with_token(&token);
+
+    // We will fetch your bot's owners and id
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| {
+            c.with_whitespace(true)
+                .on_mention(Some(bot_id))
+                .prefix(".")
+                // In this case, if "," would be first, a message would never
+                // be delimited at ", ", forcing you to trim your arguments if you
+                // want to avoid whitespaces at the start of each.
+                .delimiters(vec![", ", ","])
+                // Sets the bot's owners. These will be used for commands that
+                // are owners only.
+                .owners(owners)
+        })
+        // Set a function to be called prior to each command execution. This
+        // provides the context of the command, the message that was received,
+        // and the full name of the command that will be called.
+        //
+        // Avoid using this to determine whether a specific command should be
+        // executed. Instead, prefer using the `#[check]` macro which
+        // gives you this functionality.
+        //
+        // **Note**: Async closures are unstable, you may use them in your
+        // application if you are fine using nightly Rust.
+        // If not, we need to provide the function identifiers to the
+        // hook-functions (before, after, normal, ...).
+        .before(before)
+        // Similar to `before`, except will be called directly _after_
+        // command execution.
+        .after(after)
+        // Set a function that's called whenever an attempted command-call's
+        // command could not be found.
+        .unrecognised_command(unknown_command)
+        .group(&GENERAL_GROUP)
+        .group(&EMOJI_GROUP)
+        .group(&MATH_GROUP);
+    // Set a function that's called whenever a message is not a command.
 
     // Finally, start a single shard, and start listening to events.
     //
     // Shards will automatically attempt to reconnect, and will perform
     // exponential backoff until it reconnects.
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Err creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<CommandCounter>(HashMap::default());
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+
+        let quotes = vec![
+            BlitzQuote {quote: "Rapid and blitz chess is first of all for enjoyment.".to_string(), author: "Magnus Carlsen".to_string()},
+            BlitzQuote {quote: "Playing rapid chess, one can lose the habit of concentrating for several hours in serious chess. That is why, if a player has big aims, he should limit his rapid play in favour of serious chess.".to_string(), author: "Vladimir Kramnik".to_string()},
+            BlitzQuote {quote: "He who analyses blitz is stupid.".to_string(), author: "Rashid Nezhmetdinov".to_string()},
+            BlitzQuote {quote: "Blitz chess kills your ideas.".to_string(), author: "Bobby Fischer".to_string()},
+            BlitzQuote {quote: "To be honest, I consider [bullet chess] a bit moronic, and therefore I never play it.".to_string(), author: "Vladimir Kramnik".to_string()},
+            BlitzQuote {quote: "I play way too much blitz chess. It rots the brain just as surely as alcohol.".to_string(), author: "Nigel Short".to_string()},
+            BlitzQuote {quote: "Blitz is simply a waste of time.".to_string(), author: "Vladimir Malakhov".to_string()},
+            BlitzQuote {quote: "[Blitz] is just getting positions where you can move fast. I mean, it's not chess.".to_string(), author: "Hikaru Nakamura".to_string()},
+        ];
+
+        data.insert::<BlitzQuoteContainer>(quotes);
+    }
+
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
+}
+
+#[command]
+async fn blitz(ctx: &Context, msg: &Message) -> CommandResult {
+    let contents = String::default();
+
+    let data = ctx.data.read().await;
+    let quotes = data
+        .get::<BlitzQuoteContainer>()
+        .expect("Expected blitz quotes in typemap.");
+
+    let index;
+    {
+        let mut rng = thread_rng();
+        index = rng.gen_range(0..quotes.len());
+    }
+
+    let mut desc = String::default();
+    write!(desc, "\"{}\"", &quotes[index].quote)?;
+
+    let mut the_quote = String::default();
+    write!(the_quote, "- {}", &quotes[index].author)?;
+
+    let msg = msg
+        .channel_id
+        .send_message(&ctx.http, |m| {
+            m.embed(|e| {
+                e.color(Color::from_rgb(255, 192, 203));
+                e.description(desc);
+                e.footer(|f| {
+                    f.text(the_quote);
+                    f
+                });
+                e
+            });
+            m
+        })
+        .await
+        .expect("error making message");
+
+    msg.channel_id.say(&ctx.http, contents).await?;
+    Ok(())
+}
+
+#[command]
+// Adds multiple aliases
+#[aliases("kitty", "neko")]
+// Make this command use the "emoji" bucket.
+#[bucket = "emoji"]
+// Allow only administrators to call this:
+#[required_permissions("ADMINISTRATOR")]
+async fn cat(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, ":cat:").await?;
+
+    // We can return one ticket to the bucket undoing the ratelimit.
+    Err(RevertBucket.into())
+}
+
+#[command]
+#[description = "Sends an emoji with a dog."]
+#[bucket = "emoji"]
+async fn dog(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, ":dog:").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn bird(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let say_content = if args.is_empty() {
+        ":bird: can find animals for you.".to_string()
+    } else {
+        format!(":bird: could not find animal named: `{}`.", args.rest())
+    };
+
+    msg.channel_id.say(&ctx.http, say_content).await?;
+
+    Ok(())
+}
+
+#[command]
+// Lets us also call `~math *` instead of just `~math multiply`.
+#[aliases("*")]
+async fn multiply(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let first = args.single::<f64>()?;
+    let second = args.single::<f64>()?;
+
+    let res = first * second;
+
+    msg.channel_id.say(&ctx.http, &res.to_string()).await?;
+
+    Ok(())
+}
+
+// Commands can be created via the attribute `#[command]` macro.
+#[command]
+// Options are passed via subsequent attributes.
+// Make this command use the "complicated" bucket.
+#[bucket = "complicated"]
+async fn commands(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut contents = "Commands used:\n".to_string();
+
+    let data = ctx.data.read().await;
+    let counter = data
+        .get::<CommandCounter>()
+        .expect("Expected CommandCounter in TypeMap.");
+
+    for (k, v) in counter {
+        writeln!(contents, "- {name}: {amount}", name = k, amount = v)?;
+    }
+
+    msg.channel_id.say(&ctx.http, &contents).await?;
+
+    Ok(())
 }
